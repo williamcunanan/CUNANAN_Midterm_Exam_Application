f3668846a95455eb1722e977a2e1573097d03824
williamcunanan <145766972+williamcunanan@users.noreply.github.com>
2025-02-24 13:45:06 +0000

    Update:Displayed the data on the view in an organized manner

--- CUNANAN_Midterm_Exam_Application/routes/web.php
+++ CUNANAN_Midterm_Exam_Application/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+use App\Http\Controllers\ProductController;
+
+Route::get('/', [ProductController::class, 'index']);
